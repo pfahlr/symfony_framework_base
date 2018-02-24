0784b56c4d939de1c547fabcd8d8144d025e3c1f
Rick Pfahl <pfahlr@gmail.com>
2018-02-24 05:15:13 -0500

    HttpKernel - controller resolver

--- src/app.php
+++ src/app.php
@@ -10,6 +10,17 @@ function is_leap_year($year = null) {
   return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
 }
 
+class LeapYearController
+{
+  public function indexAction($year)
+  {
+    if(is_leap_year($year)) {
+      return new Response('Yes, this is a leap year');
+    }
+    return new Response('No, not a leap year');
+  }
+}
+
 $routes = new Routing\RouteCollection();
 
 $routes->add('hello', new Routing\Route('/hello/{name}', ['name'=>'World', '_controller'=>'render_template']));
@@ -17,12 +28,7 @@ $routes->add('bye', new Routing\Route('/bye',['_controller'=>'render_template'])
 
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
   'year'=>NULL,
-  '_controller'=>function($request){
-    if(is_leap_year($request->attributes->get('year'))) {
-      return new Response('Yes, this is a leap year');
-    }
-    return new Response('No, not a leap year');
-  }
+  '_controller'=>'LeapYearController::indexAction',
 )));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -5,6 +5,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
+
+function render_template($request) {
+  extract($request->attributes->all(),EXTR_SKIP);
+  ob_start();
+  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+  return new Response(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -13,9 +21,14 @@ $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
+$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
 try {
   $request->attributes->add($matcher->match($request->getPathInfo()));
-  $response = call_user_func($request->attributes->get('_controller'), $request);
+  $controller = $controllerResolver->getController($request);
+  $arguments = $argumentResolver->getArguments($request, $controller);
+  $response = call_user_func_array($controller, $arguments);
 }
 catch (Routing\Exception\ResourceNotFoundException $e) {
   $response = new Response('Not Found', 404);
@@ -25,10 +38,3 @@ catch (Exception $e) {
 }
 
 $response->send();
-
-function render_template($request) {
-  extract($request->attributes->all(),EXTR_SKIP);
-  ob_start();
-  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-  return new Response(ob_get_clean());
-}
