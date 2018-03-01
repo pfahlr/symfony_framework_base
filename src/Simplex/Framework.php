dbe6daaa7659157f95e8244ebf9ae71cb1b536e3
Rick Pfahl <pfahlr@gmail.com>
2018-03-01 14:36:31 -0500

    add comments

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -19,6 +19,7 @@ class Framework {
   private $controllerResolver;
   private $argumentResolver;
 
+  //called from front.php (not dependency injected).
   public function __construct(
     EventDispatcher $dispatcher,
     UrlMatcherInterface $matcher,
@@ -33,12 +34,18 @@ class Framework {
 
   public function handle(Request $request)
   {
-    $this->matcher->getContext()->fromRequest($request);
-    try {
-      $request->attributes->add($this->matcher->match($request->getPathInfo()));
-      $controller = $this->controllerResolver->getController($request);
-      $arguments = $this->argumentResolver->getArguments($request, $controller);
 
+    //getter for context member of matcher which is associated with request member, see call in front.php
+    $this->matcher->getContext()->fromRequest($request);
+
+    try {
+      //use the matcher to split the parameters from the url
+      $request->attributes->add($this->matcher->match($request->getPathInfo()));
+      //use the controller resolver to get the controller out of the request
+      $controller = $this->controllerResolver->getController($request);
+      //get the arguments from the request in context of the controller
+      $arguments = $this->argumentResolver->getArguments($request, $controller);
+      //call the controller function the builds the response
       $response = call_user_func_array($controller, $arguments);
     }
     catch (ResourceNotFoundException $e) {
@@ -53,3 +60,10 @@ class Framework {
     return $response;
   }
 }
+
+/*
+ * requiring further study:
+ *  -why does argument resolver need the controller?
+ *  -why does attributes->add() need the matcher
+ */
+
--- web/front.php
+++ web/front.php
@@ -9,32 +9,31 @@ use Symfony\Component\HttpKernel;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 use Simplex;
-/*
-function render_template($request) {
-  extract($request->attributes->all(),EXTR_SKIP);
-  ob_start();
-  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-  return new Response(ob_get_clean());
-}
-*/
 
-
+//gets the request object from php globals
 $request = Request::createFromGlobals();
+//even dispatcher object allows binding of events (required (addListener) or not (addSubscriber))
 $dispatcher = new EventDispatcher();
 //$dispatcher->addListener('response', [new Simplex\GoogleListener(),'onResponse']);
 //$dispatcher->addListener('response', [new Simplex\ContentLengthListener(),'onResponse'], -255);
 $dispatcher->addSubscriber(new Simplex\ContentLengthListener());
 $dispatcher->addSubscriber(new Simplex\GoogleListener());
 
+//include the routes
 $routes = include __DIR__.'/../src/app.php';
+//context and matcher objects required for matching routes
 $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-
+//resolves routes to their controllers
 $controllerResolver = new HttpKernel\Controller\ControllerResolver();
+//gets the arguments from the passed route
 $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
 
+//instatiate the framework
 $framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
 
+//main request handling function in the front controller
 $response = $framework->handle($request);
 
+//send the response the client
 $response->send();
