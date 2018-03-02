7b1cd886c0c738b9ec3e23eea024cf91a6443810
Rick Pfahl <pfahlr@gmail.com>
2018-03-02 04:01:19 -0500

    dependency injection / service container

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,31 +2,9 @@
 // example.com/src/Simplex/Framework.php
 namespace Simplex;
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpFoundation;
 use Symfony\Component\HttpKernel;
-use Symfony\Component\HttpFoundation\RequestStack;
-
 
 class Framework extends HttpKernel\HttpKernel
 {
-
-    public function __construct($routes)
-    {
-        $context = new Routing\RequestContext();
-        $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-        $requestStack = new RequestStack();
-
-        $controllerResolver = new HttpKernel\Controller\ControllerResolver();
-        $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-        $dispatcher = new EventDispatcher();
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-        $dispatcher->addSubscriber(new StringResponseListener());
-
-        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-    }
 }
+
--- web/front.php
+++ web/front.php
@@ -2,9 +2,29 @@
 // framework/front.php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpFoundation\Request;
+use Simplex\StringResponseListener;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
 
-//use Simplex;
+//include the service container
+$container = include __DIR__.'/../src/container.php';
+
+//configure to use StringResponseListener::class
+$container->register('listener.string_response', StringResponseListener::class);
+$container->getDefinition('dispatcher')
+    ->addMethodCall('addSubscriber', array(new Reference('listener.string_response')));
+
+//set a generic parameter
+//$container->setParameter('debug', true);
+
+//configure a ResponseListener to encode to utf-8
+$container->register('listener.response', ResponseListener::class)
+    ->setArguments(array('%charset%'));
+$container->setParameter('charset','UTF-8');
+
+//import the routes into a parameter
+$container->setParameter('routes', include __DIR__.'/../src/app.php');
 
 //gets the request object from php globals
 if(!isset($argv[0])) {
@@ -15,24 +35,8 @@ else {
     $request = Request::create(isset($argv[1]) ? $argv[1]:'/is_leap_year/2012');
 }
 
-
-//include the routes
-$routes = include __DIR__.'/../src/app.php';
-//context and matcher objects required for matching routes
-
-
-//instatiate the framework
-$framework = new Simplex\Framework($routes);
-/*
-$framework = new HttpKernel\HttpCache\HttpCache(
-    $framework,
-    new HttpKernel\HttpCache\Store(__DIR__.'/../cache'),
-    new HttpKernel\HttpCache\Esi(),
-    array('debug'=>true)
-);
-*/
 //main request handling function in the front controller
-$response = $framework->handle($request);
+$response = $container->get('framework')->handle($request);
 
 if(isset($argv[0])) var_dump($response);
 
