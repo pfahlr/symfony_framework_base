8e3719f2d6f0a4830acd8770e41094ab6099600f
Rick Pfahl <pfahlr@gmail.com>
2018-03-01 23:46:29 -0500

    move front controller logic into framework constructor

--- web/front.php
+++ web/front.php
@@ -2,12 +2,7 @@
 // framework/front.php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\Routing;
 
 //use Simplex;
 
@@ -20,35 +15,14 @@ else {
     $request = Request::create(isset($argv[1]) ? $argv[1]:'/is_leap_year/2012');
 }
 
-$requestStack = new RequestStack();
 
 //include the routes
 $routes = include __DIR__.'/../src/app.php';
 //context and matcher objects required for matching routes
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-//resolves routes to their controllers
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-//gets the arguments from the passed route
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-
-//event dispatcher object allows binding of events (required (addListener) or not (addSubscriber))
-$dispatcher = new EventDispatcher();
-//$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-//$dispatcher->addSubscriber(new Simplex\GoogleListener());
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ExceptionListener(
-    'Calendar\Controller\ErrorController::exceptionAction'
-));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
 
 //instatiate the framework
-$framework = new Simplex\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+$framework = new Simplex\Framework($routes);
 /*
 $framework = new HttpKernel\HttpCache\HttpCache(
     $framework,
